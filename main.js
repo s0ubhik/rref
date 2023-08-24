0fbe81c960d49a58e15e17ef783d8a04e66a7ed0
Soubhik Biswas <soubhik1809@gmail.com>
2023-08-24 05:34:39 +0530

    replaced input field from output

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ function rref(x){
     for (let i = 0; i < mat_size[1] - 1; i++) {
         if (i >= mat_size[1] || i >= mat_size[0]) continue;
         let c = x[i][i];
-        console.log(i);
         if (c === 0) continue;
 
         output += "<div class=\"msg\">Divide all elements of R"+(i+1)+" with the "+ordinal_suffix_of(i+1)+" element of R"+(i+1)+"<br></div>"
